07f886f71481d275c50dd1b3b8fcc16f94f26eda
zhenwei1108 <zhenwei1108@163.com>
2022-12-22 20:34:15 +0800

    update baseMessage

--- src/main/java/com/github/wegoo/network/engine/BaseMessage.java
+++ src/main/java/com/github/wegoo/network/engine/BaseMessage.java
@@ -3,17 +3,12 @@ package com.github.wegoo.network.engine;
 /**
  * @author: zhangzhenwei 
  * @description: BaseMessage
- *  消息处理接口类
+ *  消息处理基类
  * @date: 2022/12/21  15:13
  * @since: 1.0.0 
  */
 public interface BaseMessage {
-  long getMessageId();
 
-  void setData(byte[] data);
-
-  byte[] getData();
-
-  String toString();
+  byte[] getEncode();
 
 }
