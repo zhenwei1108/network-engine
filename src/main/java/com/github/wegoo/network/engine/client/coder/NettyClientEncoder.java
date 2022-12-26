6d286f3b782571b44f2f6ef30e795991248c6d42
zhenwei1108 <zhenwei1108@163.com>
2022-12-26 21:35:03 +0800

    add desc

--- src/main/java/com/github/wegoo/network/engine/client/coder/NettyClientEncoder.java
+++ src/main/java/com/github/wegoo/network/engine/client/coder/NettyClientEncoder.java
@@ -7,6 +7,13 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 import lombok.AllArgsConstructor;
 
+/**
+ * @author: zhangzhenwei 
+ * @description: NettyClientEncoder
+ *  客户端，发送消息时编码
+ * @date: 2022/12/26  21:27
+ * @since: 1.0.0 
+ */
 @AllArgsConstructor
 public class NettyClientEncoder extends MessageToByteEncoder<BaseMessage> {
 
